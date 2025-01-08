9c5247941bda9df186e9c8a18b029c8a5a2d9712
Jean Paul <131689163+origemjhanpoll@users.noreply.github.com>
2025-01-08 17:48:06 +0000

    rename header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ const Header: React.FC = () => {
       <nav>
         <ul style={styles.navList}>
           <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="about" smooth={true} duration={500}>Sobre</Link></li>
-          <li><Link to="projects" smooth={true} duration={500}>Projetos</Link></li>
-          <li><Link to="contact" smooth={true} duration={500}>Contato</Link></li>
+          <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+          <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
+          <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
         </ul>
       </nav>
     </header>
